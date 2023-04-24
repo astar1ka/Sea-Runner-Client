351510bb0c087134343213b8a120353f12544013
dvntcxz <r.babyboy007@gmail.com>
2023-04-24 23:47:13 +0400

    bl ne tak sohranil

--- src/pages/mainPage/login/Login.tsx
+++ src/pages/mainPage/login/Login.tsx
@@ -26,21 +26,19 @@ export default function Login(props: any) {
                     <img className='login-logo-image' src={logo}/>
                 </div>
                 <div className="window-elems">
-                <form action="">
                     <h2 className="h2-login">Sea Runner</h2>
-                    <div className="inputbox">
+                    <div className="login-inputbox">
                         <i className="icon-user"></i>
                         <input ref={login} placeholder=' ' required/>    
                         <label htmlFor='login'>Логин</label>
                     </div>
-                    <div className="inputbox" id="passIn"> 
+                    <div className="login-inputbox"> 
                         <i className="icon-lock"></i>   
                         <input  type="password" ref={password} placeholder=' ' required/>
                         <label htmlFor='password'>Пароль</label>
                     </div>
                     <button className="loginButton" onClick={loginHandler}>Войти</button>
                     <p className={'errorLogin' + ((loginStatus) ? ' hide' : '')}>Неверный логин или пароль</p>
-                    </form>
                 </div>
             </div>
         </div>
