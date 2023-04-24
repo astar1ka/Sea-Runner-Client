a85813dbf9fa88f1680668357912e19fe43f4b85
dvntcxz <r.babyboy007@gmail.com>
2023-04-24 18:02:08 +0400

    verska
    хз, не перебрасывает, там чот с сервером

--- src/hooks/useSubcriber.tsx
+++ src/hooks/useSubcriber.tsx
@@ -1,5 +1,6 @@
 export default function useSubcriber(callbackOnTrue:Function, callbackOnFalse:Function) {
+    
     return (result: boolean, data: any = null) => {
-        (result) ? callbackOnTrue(data) : callbackOnFalse(data);
+        (result) ? callbackOnTrue(data) : callbackOnFalse(data);    
     }
 }
